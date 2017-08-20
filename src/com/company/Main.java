9b69d9c2b186dc5bfc06e0ed4b4f0d9920345462
bservice <brservice@comcast.net>
2017-08-20 19:07:18 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 static void playGame() {
+        int computersNumber; 
+        int usersGuess;      
+        int guessCount;     
+        computersNumber = (int)(100 * Math.random()) + 1;
+           
+        guessCount = 0;
+        System.out.println();
+        System.out.print("What is your first guess? ");
+        while (true) {
+            usersGuess = TextIO.getInt();  
+            guessCount++;
+            if (usersGuess == computersNumber) {
+                System.out.println("Number of guesses was " + guessCount
+                        + "  The number was " + computersNumber);
+                break;  
+            }
+            if (guessCount == 9) {
+                System.out.println("You didn't get the number");
+                System.out.println("You lose. Number was " + computersNumber);
+                break; 
+            }
+           
+            if (usersGuess < computersNumber)
+                System.out.print("Too low ");
+            else if (usersGuess > computersNumber)
+                System.out.print("Too high ");
+        }
+        System.out.println();
+    } 
+
+} 
     }
 }
